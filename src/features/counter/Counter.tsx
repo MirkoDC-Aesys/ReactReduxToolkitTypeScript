0063833f88fc3113fb725bb4691eebb109896cf0
MirkoDC-Aesys <85115982+MirkoDC-Aesys@users.noreply.github.com>
2021-07-06 16:07:08 +0200

    Add Counter.tsx

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,68 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react'
+import styles from './Counter.module.css'
+import { useAppDispatch, useAppSelector } from '../../app/hooks'
+import { incremented, decremented } from './counterSlice'
 
-import { useAppSelector, useAppDispatch } from '../../app/hooks';
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  incrementIfOdd,
-  selectCount,
-} from './counterSlice';
-import styles from './Counter.module.css';
-
-export function Counter() {
-  const count = useAppSelector(selectCount);
-  const dispatch = useAppDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
-
-  const incrementValue = Number(incrementAmount) || 0;
+export default function Counter() {
+  const count = useAppSelector(state => state.counter.value)
+  const dispatch = useAppDispatch()
 
   return (
-    <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
+    <section>
+      <h2 className={styles.title}>Counter</h2>
+      <div>
+        <button onClick={() => dispatch(decremented())}>-</button>
+        <span className={styles.count}>{count}</span>
+        <button onClick={() => dispatch(incremented())}>+</button>
       </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
-        </button>
-      </div>
-    </div>
-  );
+    </section>
+  )
 }
--- src/features/counter/counterSlice.ts
+++ src/features/counter/counterSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 interface CounterState {
   value: number;
@@ -12,14 +12,14 @@ export const counterSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
-    increment: (state) => {
+    incremented: (state) => {
       state.value += 1;
     },
-    decrement: (state) => {
+    decremented: (state) => {
       state.value -= 1;
     }
   }
 })
 
-export const { increment, decrement } = counterSlice.actions;
+export const { incremented, decremented } = counterSlice.actions;
 export default counterSlice.reducer;
