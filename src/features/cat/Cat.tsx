bcfd315faf87d53ab5b1dc2850e3b1914d914db9
MirkoDC-Aesys <85115982+MirkoDC-Aesys@users.noreply.github.com>
2021-07-07 12:54:51 +0200

    Add refetch in Cat component

--- src/features/cat/Cat.tsx
+++ src/features/cat/Cat.tsx
@@ -1,17 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../../components/Button'
 import styles from './Cat.module.css'
 import { useGetCatQuery } from './catSlice'
 
 export default function Cat() {
-
-    const { data = [], error, isLoading } = useGetCatQuery()
+    const { data = [], error, isLoading, isFetching, refetch } = useGetCatQuery()
 
     return (
         <section>
-            Cat
+            <button onClick={refetch} >Cat</button>
             <div>
-                <img className={styles.cat} src={isLoading ? 'https://media1.tenor.com/images/8f7a28e62f8242b264c8a39ba8bea261/tenor.gif?itemid=15922897' : data[0].url} alt="" />
+                <img className={styles.cat} src={isLoading || isFetching ? 'https://media1.tenor.com/images/8f7a28e62f8242b264c8a39ba8bea261/tenor.gif?itemid=15922897' : data[0].url} alt="" />
             </div>
         </section>
     )
